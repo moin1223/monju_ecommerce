fa5b1fa5059f9167ae330e97138633b30c83185a
moin1223 <moinpuccse@gmail.com>
2024-07-05 00:49:23 +0600

    test

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -28,7 +28,7 @@
         </div>
 
 
-        <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 row-cols-sm-2 g-3 g-lg-3 mb-5">
+        {{-- <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 row-cols-sm-2 g-3 g-lg-3 mb-5">
 
             @foreach ($products as $key => $product)
                 <div class="col products">
@@ -72,7 +72,50 @@
                 </div>
             @endforeach
 
+        </div> --}}
+        <div class="row row-cols-2 row-cols-lg-4 row-cols-md-2 g-3 g-lg-3 mb-5">
+            @foreach ($products as $key => $product)
+                <div class="col products">
+                    <div class="p-2 productsImg position-relative">
+                        <img src="{{$product->image}}" alt="" />
+                        @if($product->stock == "no")
+                            <div class="text-overlay">Stock Out</div>
+                        @endif
+                        <div class="my-6 productsText">
+                            {{-- <h4>{{ $product->product_name}}</h4> --}}
+                            <h4>{{ \Illuminate\Support\Str::limit($product->product_name, 10, '..') }}</h4>
+
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex align-items-center w-100">
+                                    <span class="text-decoration-line-through">৳{{ $product->old_price }}</span>
+                                    <span class="presentPrice">৳{{ $product->new_price }}</span>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- <span class="my-4">{{ $product->weight }}</span> --}}
+                        <div class="position-absoulate hoverProducts">
+                            <div class="d-flex">
+                                <form action="{{ route('cart.add', $product->id) }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="buttonBgColor addToCart" id="addToCart" {{ $product->stock == "no" ? 'disabled' : '' }}>
+                                        <i class="bi bi-basket-fill cartIcon"></i> Add
+                                    </button>
+                                </form>
+                                <a href="{{ route('product-details',$product->id) }}">
+                                    <button class="buttonBgColor addToCart">Details</button>
+                                </a>
+                            </div>
+                        </div>
+                        <a href="{{ route('order.create',$product->id) }}">
+                            <button class="orderNow" id="orderNow" {{ $product->stock == "no" ? 'disabled' : '' }}>অর্ডার করুন</button>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
         </div>
+        
+
+
     </div>
 @endsection
 @section('js')
